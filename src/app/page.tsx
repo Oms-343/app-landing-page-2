cbcf88a5bb03c0cafc4f4e1f3d5d46916c66deb3
Oms-343 <oms87473@gmail.com>
2024-09-03 14:09:46 +0530

    Header section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "../sections/Header";
+
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      <h1>hello world</h1>
+    <div className="">
+      <Header />
     </div>
   );
 }
---
+++ src/sections/Header.tsx
@@ -0,0 +1,33 @@
+import LogoIcon from "@/public/Logo.svg";
+import UserIcon from "@/public/User.svg";
+import MenuIcon from "@/public/Menu.svg";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-20 py-4">
+      <div className="container flex items-center justify-between">
+        <div className="flex items-center">
+          <LogoIcon className="h-10 w-10" />
+
+          <nav className="ml-[74px] hidden gap-14 desktop:flex">
+            <a href="#">Features</a>
+            <a href="#">Pricing</a>
+            <a href="#">Enterprise</a>
+            <a href="#">Careers</a>
+          </nav>
+        </div>
+
+        <div className="flex items-center gap-5 desktop:gap-14">
+          <p className="hidden desktop:block">Open an Account</p>
+          <p className="flex items-center gap-1">
+            <UserIcon className="h-8 w-8" />
+            <span className="hidden desktop:inline">Sign in</span>
+          </p>
+          <button>
+            <MenuIcon className="h-8 w-8 desktop:hidden" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
