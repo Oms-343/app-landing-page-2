ce1488d422246d7ccfabde924fdcf4a2d7d61faa
Oms-343 <oms87473@gmail.com>
2024-09-04 16:01:45 +0530

    Pricing section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/sections/Hero";
 import Features from "@/sections/Features";
 import FAQs from "@/sections/FAQs";
+import Pricing from "@/sections/Pricing";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Features />
       <FAQs />
+      <Pricing />
     </div>
   );
 }
